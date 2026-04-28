4627a3d89c18b1001dbd25846359b8724d52d028
rishika <rishika@C010422930.zipangu.cac.co.jp>
2026-04-28 15:49:52 +0900

    4/28終了分

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question02/SystemMain02.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question02/SystemMain02.java
@@ -3,7 +3,12 @@ package q01_basic.question02;
 public class SystemMain02 {
 
 	public static void main(String[] args) {
-		//TODO ここから実装する
+		Member member = new Member();
+		member.setAge(30);
+		member.setName("Miura Manabu");
+		member.setRank(1);
+		member.showMember();
+		member.rankUp();
 
 	}
 }
